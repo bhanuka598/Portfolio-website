f3881e61c5c7e9fcd3df34a4b3ed6bcaad222544
Bhanuka Pawan Athukorala <bhanuka_pawan@yahoo.com>
2026-05-03 19:45:06 +0530

    Update skills
    Now can update skills from skills.js. Skills are Marqueeing

--- client/src/sections/skills/SkillBadge.jsx
+++ client/src/sections/skills/SkillBadge.jsx
@@ -1,9 +1,9 @@
-function SkillBadge({ skill, Icon, colorClass }) {
+function SkillBadge({ skill }) {
+    const { name, icon: Icon, color } = skill
   return (
     <div className='inline-flex items-center gap-2 bg-white border border-slate-200 px-3 py-1 rounded-full text-sm font-medium hover:shadow-sm transition-shadow'>
-      {/* Icon with its official brand color */}
-      {Icon && <Icon className={colorClass} size={18} />}
-      <span className="text-slate-700">{skill}</span>
+        {Icon && <Icon className={color} size={38} />}
+        <span className="text-slate-700">{name}</span>
     </div>
   )
 }
